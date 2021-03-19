6ca4d55f3a783c2d2bbfff1a5de82a4ea8e715d1
YanZhiwei <Yan.Zhiwei@hotmail.com>
2021-03-19 18:51:29 +0800

    增加单元测试，以及支持target frameworks (#4)

---
+++ XiangJiang.Infrastructure.Orm.DapperTests/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿namespace XiangJiang.Infrastructure.Orm.DapperTests.Models
+{
+    public sealed class Company
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Address { get; set; }
+
+        public float Salary { get; set; }
+    }
+}
