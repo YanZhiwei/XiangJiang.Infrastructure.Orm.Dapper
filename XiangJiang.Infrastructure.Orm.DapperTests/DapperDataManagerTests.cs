a087ef5c48398b33aedef6469a9af796bb6e9032
言志伟 <Yan.Zhiwei@hotmail.com>
2021-03-19 18:37:52 +0800

    增加单元测试，以及支持target frameworks

--- XiangJiang.Infrastructure.Orm.DapperTests/DapperDataManagerTests.cs
+++ XiangJiang.Infrastructure.Orm.DapperTests/DapperDataManagerTests.cs
@@ -1,7 +1,9 @@
 ﻿using System.IO;
 using Microsoft.Data.Sqlite;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XiangJiang.Common;
 using XiangJiang.Infrastructure.Orm.Dapper;
+using XiangJiang.Infrastructure.Orm.DapperTests.Models;
 
 namespace XiangJiang.Infrastructure.Orm.DapperTests
 {
@@ -17,12 +19,12 @@ namespace XiangJiang.Infrastructure.Orm.DapperTests
             if (File.Exists(SqlitePath))
                 File.Delete(SqlitePath);
             _dapperDataManager = new DapperDataManager($"Data Source={SqlitePath}", SqliteFactory.Instance);
+            CreateTable();
         }
 
-        [TestMethod]
-        public void ExecuteNonQueryTest()
+        private void CreateTable()
         {
-            var createTable = @"CREATE TABLE COMPANY(
+            var createTable = @"CREATE TABLE company(
    ID INT PRIMARY KEY     NOT NULL,
    NAME           TEXT    NOT NULL,
    AGE            INT     NOT NULL,
@@ -30,6 +32,22 @@ namespace XiangJiang.Infrastructure.Orm.DapperTests
    SALARY         REAL
 );";
             var actual = _dapperDataManager.ExecuteNonQuery(createTable);
+            Assert.IsTrue(actual >= 0);
+        }
+
+        [TestMethod]
+        public void ExecuteNonQueryTest()
+        {
+            var sql = "insert into company values(@Id,@Name,@Age,@Address,@Salary)";
+            var company = new Company
+            {
+                Address = "shanghai",
+                Age = RandomHelper.NextNumber(0, 100),
+                Id = RandomHelper.NextNumber(0, 100),
+                Name = RandomHelper.NextHexString(4),
+                Salary = 13.23f
+            };
+            var actual = _dapperDataManager.ExecuteNonQuery(sql, company);
             Assert.IsTrue(actual >= 0);
         }
     }
